b7b12273c0585be1271fd5095202779efc6161f9
emanherawy <eman.herawy@gmail.com>
2022-10-22 19:12:58 +0200

    fix/return back to comming soon

---
+++ js/textile-tools-public-js-wptextileplugin.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+    var data = TEXTILE_AJAX_OBJ ? TEXTILE_AJAX_OBJ : {};
+    var textile = new WPTextilePlugin(data);
+
+});
+
+var WPTextilePlugin = (
+    function($) {
+        var constructor = function(data) {
+           
+        }
+
+
+        return constructor;
+    }
+)(jQuery);
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
